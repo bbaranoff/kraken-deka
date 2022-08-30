b39a95c2459639b52835bb91847667adcd440b7a
bastien baranoff <37385191+bbaranoff@users.noreply.github.com>
2022-08-30 15:11:03 +0200

    Update and rename behemooth to behemooth.sh

--- behemooth.sh
+++ behemooth.sh
@@ -4,7 +4,7 @@ echo 0 > test
 for abblay in $echo $(ls /media/nirvana/tables) ; do abblay2=$(echo $abblay | sed 's/.dlt//g');
 cd /media/nirvana/indexes/
 /media/nirvana/indexes/kraken/TableConvert/TableConvert di /media/nirvana/tables/$abblay2.dlt $abblay2.ins:$offset_total $abblay2.idx
-taille_arrondie=$(echo $(( $(stat -c%s 100.ins)/4 )) *4 | bc)
+taille_arrondie=$(echo $(( $(stat -c%s $abblay2.ins)/4 )) *4 | bc)
 offset_total=$(($taille_arrondie + $offset_total))
-echo $offset_total
+echo $taille_arrondie >> test
 done
