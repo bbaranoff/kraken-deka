f5d75045701118076696833af22334c26e062978
bastien baranoff <37385191+bbaranoff@users.noreply.github.com>
2022-09-01 22:58:17 +0200

    Update behemooth.sh

--- behemooth.sh
+++ behemooth.sh
@@ -4,7 +4,7 @@ echo 0 > test
 for abblay in $echo $(ls /media/$USER/tables) ; do abblay2=$(echo $abblay | sed 's/.dlt//g');
 cd /media/$USER/indexes/
 /media/$USER/indexes/kraken/TableConvert/TableConvert di /media/$USER/tables/$abblay2.dlt $abblay2.ins:$offset_total $abblay2.idx
-taille_arrondie=$(echo $(( $(( $(stat -c%s $abblay2.ins)/4096 )) +1 )) *4096 | bc)
+taille_arrondieecho $(( $(( $(stat -c%s $abblay2.ins)/4096 )) +1))
 offset_total=$(($taille_arrondie + $offset_total))
 echo $taille_arrondie >> test
 done
